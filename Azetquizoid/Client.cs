7c6cf8900ad6d6c1f0376d8d792c0fc03e22aa81
fireantik <windowmaker001@gmail.com>
2015-06-14 11:39:53 +0200

    init

---
+++ Azetquizoid/Client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azetquizoid
+{
+    public abstract class WSClient
+    {
+        public WebSocket Socket;
+
+        public abstract void Opened();
+        public abstract void Closed();
+        public abstract void Received(string message);
+
+        public async Task SendAsync(string message)
+        {
+            var bytes = Encoding.UTF8.GetBytes(message);
+            var segment = new ArraySegment<byte>(bytes);
+            await Socket.SendAsync(segment, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+        public void Send(string message)
+        {
+            SendAsync(message).Wait();
+        }
+
+        public void Close()
+        {
+
+        }
+    }
+
+    public class EchoClient : WSClient
+    {
+        public override void Opened()
+        {
+        }
+
+        public override void Closed()
+        {
+        }
+
+        public override void Received(string message)
+        {
+            Send(message);
+        }
+    }
+}
---
+++ Azetquizoid/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Azetquizoid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port;
+            if (!int.TryParse(Environment.GetEnvironmentVariable("port"), out port) && !int.TryParse(Environment.GetEnvironmentVariable("PORT"), out port)) port = 3000;
+            var server = new SimpleHTTPServer("Content", 3000, new Tuple<string, Type>("/echo", typeof(EchoClient)));
+            Console.ReadLine();
+        }
+    }
+}
