4918069857283256503f1861d45b91ad58189812
fireantik <windowmaker001@gmail.com>
2015-06-14 12:15:41 +0200

    mono solution

--- Program.cs
+++ Program.cs
@@ -10,14 +10,14 @@ using System.Threading;
 
 namespace Azetquizoid
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int port;
-            if (!int.TryParse(Environment.GetEnvironmentVariable("port"), out port) && !int.TryParse(Environment.GetEnvironmentVariable("PORT"), out port)) port = 3000;
-            var server = new SimpleHTTPServer("Content", 3000, new Tuple<string, Type>("/echo", typeof(EchoClient)));
-            Console.ReadLine();
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int port;
+			if (!int.TryParse(Environment.GetEnvironmentVariable("port"), out port) && !int.TryParse(Environment.GetEnvironmentVariable("PORT"), out port)) port = 3000;
+			var server = new SimpleHTTPServer("Content", 3000, new Tuple<string, Type>("/echo", typeof(EchoClient)));
+			Console.ReadLine();
+		}
+	}
 }
