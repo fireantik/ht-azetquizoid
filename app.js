3c3e86ba6e1b64191f03aac6248b1d3c680463bc
fireantik <windowmaker001@gmail.com>
2015-06-14 13:42:04 +0200

    express ws

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var expressWs = require('express-ws')(app);
+
 var port = process.env.PORT || process.env.port || 3000;
 
 app.use(express.static('public'));
@@ -8,11 +10,16 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
+app.ws('/echo', function (ws, req) {
+    ws.on('message', function (msg) {
+        ws.send(msg);
+    });
+});
+
 var server = app.listen(port, function () {
 
     var host = server.address().address;
     var port = server.address().port;
 
     console.log('Example app listening at http://%s:%s', host, port);
-
 });
